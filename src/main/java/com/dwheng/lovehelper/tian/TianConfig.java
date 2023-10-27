0dcccdd37868a08f6a6c1e169a54298fa93cd7e6
Heath <duweihengmr@163.com>
2023-10-27 16:15:32 +0800

    feat: add tianapi

---
+++ src/main/java/com/dwheng/lovehelper/tian/TianConfig.java
@@ -0,0 +1,18 @@
+package com.dwheng.lovehelper.tian;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: dwh
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "tian")
+public class TianConfig {
+    private String key;
+    private String dujitangUrl;
+    private String zaoanUrl;
+    private String wananUrl;
+}
---
+++ src/main/java/com/dwheng/lovehelper/tian/TianService.java
@@ -0,0 +1,69 @@
+package com.dwheng.lovehelper.tian;
+
+import com.alibaba.fastjson.JSONObject;
+import com.dwheng.lovehelper.utils.OkHttpUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: dwh
+ **/
+@Service
+@Slf4j
+public class TianService {
+
+    @Resource
+    TianConfig tianConfig;
+
+    public String getDujitang() {
+        String url = tianConfig.getDujitangUrl() +
+                "?key=" + tianConfig.getKey();
+        try {
+            String response = OkHttpUtils.getResponse(url);
+            JSONObject jsonObject = JSONObject.parseObject(response);
+            if (null != jsonObject) {
+                String caihongpi = jsonObject.getJSONObject("result").getString("content");
+                return caihongpi;
+            }
+        } catch (Exception e) {
+            log.error("彩虹屁获取失败:{} {}", e.getMessage(), e.getStackTrace());
+        }
+        return null;
+    }
+
+    public String getZaoan() {
+        String url = tianConfig.getZaoanUrl() +
+                "?key=" + tianConfig.getKey();
+        try {
+            String response = OkHttpUtils.getResponse(url);
+            JSONObject jsonObject = JSONObject.parseObject(response);
+            if (null != jsonObject) {
+                String caihongpi = jsonObject.getJSONObject("result").getString("content");
+                return caihongpi;
+            }
+        } catch (Exception e) {
+            log.error("早安获取失败:{} {}", e.getMessage(), e.getStackTrace());
+        }
+        return null;
+    }
+
+    public String getWanan() {
+        String url = tianConfig.getWananUrl() +
+                "?key=" + tianConfig.getKey();
+        try {
+            String response = OkHttpUtils.getResponse(url);
+            JSONObject jsonObject = JSONObject.parseObject(response);
+            if (null != jsonObject) {
+                String caihongpi = jsonObject.getJSONObject("result").getString("content");
+                return caihongpi;
+            }
+        } catch (Exception e) {
+            log.error("晚安获取失败:{} {}", e.getMessage(), e.getStackTrace());
+        }
+        return null;
+    }
+
+
+}
